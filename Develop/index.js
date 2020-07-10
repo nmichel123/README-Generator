9f69292e4099f56cc5b4c998393963d953a3d217
Nico Michel <nicomichel@utexas.edu>
2020-07-09 19:27:28 -0500

    added files to gitignore; finished prompted questions NOT YET TESTED

--- Develop/index.js
+++ Develop/index.js
@@ -1,6 +1,51 @@
 // array of questions for user
 const questions = [
-
+{
+    type: input,
+    message: "Title of Project?",
+    name: "title",
+},
+{
+    type: input,
+    message: "Description of Project?",
+    name: "description",
+},
+{
+    type: list,
+    message: "List Table of Contents",
+    name: "toc",
+},
+{
+    type: input,
+    message: "Installation Instructions?",
+    name: "installation"
+},
+{
+    type: input,
+    message: "Usage Information?",
+    name: "usage",
+},
+{
+    type: list,
+    message: "License Information?",
+    choices: ["Free to use", "Use with Permission", "No one can use",],
+    name: "license",
+},
+{
+    type: input,
+    message: "Contribution Guidelines?",
+    name: "contribution",
+},
+{
+    type: input,
+    message: "Testing Information?",
+    name: "test",
+},
+{
+    type: input,
+    message: "Contact information for Questions?",
+    name: "contact",
+},
 ];
 
 // function to write README file
