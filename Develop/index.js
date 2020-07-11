0bc8208b7baf287e0c193b4d6316e3619dfc9e60
Nico Michel <nicomichel@utexas.edu>
2020-07-10 19:32:36 -0500

    edited questions and wrote prompt function to display successfully

--- Develop/index.js
+++ Develop/index.js
@@ -1,52 +1,67 @@
+const inquirer = require("inquirer");
+const fs = require("fs"); 
+const util = require("util");
+const axios = require("axios");
+
+const readFileAsync = util.promisify(fs.readFile);
+const writeFileAsync = util.promisify(fs.writeFile);
+
 // array of questions for user
-const questions = [
-{
-    type: input,
+const questions = 
+[{
+    type: "input",
     message: "Title of Project?",
     name: "title",
 },
 {
-    type: input,
+    type: "input",
     message: "Description of Project?",
     name: "description",
 },
 {
-    type: list,
-    message: "List Table of Contents",
-    name: "toc",
-},
-{
-    type: input,
+    type: "input",
     message: "Installation Instructions?",
     name: "installation"
 },
 {
-    type: input,
+    type: "input",
     message: "Usage Information?",
     name: "usage",
 },
 {
-    type: list,
+    type: "list",
     message: "License Information?",
     choices: ["Free to use", "Use with Permission", "No one can use",],
     name: "license",
 },
 {
-    type: input,
+    type: "input",
     message: "Contribution Guidelines?",
     name: "contribution",
 },
 {
-    type: input,
+    type: "input",
     message: "Testing Information?",
     name: "test",
 },
 {
-    type: input,
-    message: "Contact information for Questions?",
+    type: "input",
+    message: "Contact Email?",
     name: "contact",
 },
+{
+    type: "input",
+    message: "GitHub Username?",
+    name: "gitHub",
+},
 ];
+prompt()
+
+function  prompt() {
+ return inquirer.prompt(questions);
+}
+
+writeToFile ()
 
 // function to write README file
 function writeToFile(fileName, data) {
